5965157e1450768fc756d7c073edc72e3d7adc82
TakumaNishimura <tak.nishimura.0723@gmail.com>
2015-02-27 18:22:13 +0900

    add search.php

--- 404.php
+++ 404.php
@@ -1,17 +1,11 @@
-<% include _header %>
+<?php get_header(); ?>
 
     <article class="notFound">
       <h2 class="notFound__title">お探しのページは見つかりませんでした</h2>
-      <!-- For 404 -->
-      <!-- <p class="notFound__body">
-        <a href="#">TOP</a>に戻って最新記事をご覧ください。
-      </p> -->
-      <!-- For Search -->
       <p class="notFound__body">
-        「SearchWord」の検索結果に当てはまる記事は見つかりませんでした。<br />
-        <a href="#">TOP</a>に戻って最新記事をご覧くだくか、他のキーワードで検索をしてください。
+        <a href="<?php bloginfo('url') ?>">TOP</a>に戻って最新記事をご覧ください。
       </p>
     </article>
 
-    <% include _sidebar %>
-<% include _footer %>
+    <?php get_sidebar(); ?>
+<?php get_footer(); ?>
