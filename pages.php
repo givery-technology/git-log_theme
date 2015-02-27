0a2741679e6d57c02f3e6cd6cf33cbc99c33ef1f
TakumaNishimura <tak.nishimura.0723@gmail.com>
2015-02-27 16:37:28 +0900

    post page

--- pages.php
+++ pages.php
@@ -1,23 +1,18 @@
 <?php get_header(); ?>
 
     <article class="singlePost">
+      <?php while (have_posts()) : the_post(); ?>
       <div class="singlePost__info">
       </div>
-      <h2 class="singlePost__postTitle">[SLab.]CSSを爆速コーディング！今すぐ始められる「Sass」＆「Compass」入門！【Compass入門編】</h2>
+      <h2 class="singlePost__postTitle"><?php the_title(); ?></h2>
       <div class="singlePost__thumbnail singlePost__thumbnail--pages">
-        <img src="images/pages-sample.png" alt="thumbnail">
+        <?php the_post_thumbnail(); ?>
       </div>
       <div class="giveryMission">Your Growth, Our Future.</div>
       <section class="singlePost__body">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto nostrum obcaecati quo id asperiores maxime molestiae amet maiores incidunt, excepturi, eum eligendi vitae mollitia, commodi adipisci cupiditate quaerat nisi nihil.</p>
-        <p><a href="#">Lorem ipsum dolor sit amet</a>, consectetur adipisicing elit. Architecto nostrum obcaecati quo id asperiores maxime molestiae amet maiores incidunt, excepturi, eum eligendi vitae mollitia, commodi adipisci cupiditate quaerat nisi nihil.</p>
-        <h3>初の海外！初のアメリカ！何もかもが新鮮でした！</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto nostrum obcaecati quo id asperiores maxime molestiae amet maiores incidunt, excepturi, eum eligendi vitae mollitia, commodi adipisci cupiditate quaerat nisi nihil.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto nostrum obcaecati quo id asperiores maxime molestiae amet maiores incidunt, excepturi, eum eligendi vitae mollitia, commodi adipisci cupiditate quaerat nisi nihil.</p>
-        <h4>初の海外！初のアメリカ！何もかもが新鮮でした！</h4>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto nostrum obcaecati quo id asperiores maxime molestiae amet maiores incidunt, excepturi, eum eligendi vitae mollitia, commodi adipisci cupiditate quaerat nisi nihil.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto nostrum obcaecati quo id asperiores maxime molestiae amet maiores incidunt, excepturi, eum eligendi vitae mollitia, commodi adipisci cupiditate quaerat nisi nihil.</p>
+        <?php the_content(); ?>
       </section>
+      <?php endwhile; ?>
     </article>
 
     <?php get_sidebar(); ?>
