4cf4b99bff79e24bfea0e6613e9e1710ee031454
TakumaNishimura <tak.nishimura.0723@gmail.com>
2015-02-27 19:56:24 +0900

    delete coffee ejs

--- header.php
+++ header.php
@@ -10,16 +10,12 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
   <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
-  <script src="script/script.js"></script>
   <?php wp_head(); ?>
   <title>
     <?php if(is_home()): ?>
       <?php bloginfo('name'); ?>
     <?php endif; ?>
     <?php if(is_single()): ?>
-      <?php single_post_title(); ?>｜<?php bloginfo('name'); ?>
-    <?php endif; ?>
-    <?php if(is_page()): ?>
       <?php single_post_title(); ?>｜<?php bloginfo('name'); ?>
     <?php endif; ?>
     <?php if(is_tag()): ?>
