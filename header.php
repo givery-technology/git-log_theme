96ddecac826c868ab7fd753904db0b38a32142dc
TakumaNishimura <tak.nishimura.0723@gmail.com>
2015-03-10 19:08:46 +0900

    add Google analytics

--- header.php
+++ header.php
@@ -11,6 +11,16 @@
   <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
   <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-56063080-4', 'auto');
+    ga('send', 'pageview');
+
+  </script>
   <?php wp_head(); ?>
   <title>
     <?php if(is_home()): ?>
